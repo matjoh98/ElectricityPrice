b2027be9da8aef702102dfb3fcd08b477da4611e
Johansson Mattias <Mattias.Johansson@volvo.com>
2023-03-14 19:53:43 +0100

    New

---
+++ main.py
@@ -0,0 +1,14 @@
+import json
+import http.client
+import plotly.express as px
+
+if __name__ == "__main__":
+
+    conn = http.client.HTTPSConnection("www.elprisetjustnu.se")
+    payload = ""
+    conn.request("GET", "/api/v1/prices/2023/03-14_SE3.json", payload)
+    res = conn.getresponse()
+
+    data = json.loads(res.read().decode())
+    fig = px.line(y=[i["SEK_per_kWh"] for i in data2], x=[i["time_start"] for i in data2])
+    fig.write_html("index.html")
