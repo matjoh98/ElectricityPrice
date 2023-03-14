a11f6d9e86903dffb4603b893937ea7fded29566
Mattias Johansson <Mattias.Johansson@volvo.com>
2023-03-14 21:18:48 +0100

    Update time

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-import json
 import http.client
+import json
+from datetime import datetime, timedelta
+
 import plotly.express as px
+
+
+def get_dates():
+    presentday = datetime.now()  # or presentday = datetime.today()
+    # Get Tomorrow
+    tomorrow = presentday + timedelta(1)
+    year = tomorrow.strftime("%d-%m-%Y").split("-")[2]
+    month = tomorrow.strftime("%d-%m-%Y").split("-")[1]
+    day = tomorrow.strftime("%d-%m-%Y").split("-")[0]
+    return year, month, day
+
 
 if __name__ == "__main__":
 
     conn = http.client.HTTPSConnection("www.elprisetjustnu.se")
     payload = ""
-    conn.request("GET", "/api/v1/prices/2023/03-14_SE3.json", payload)
+
+    year, month, day = get_dates()
+    conn.request("GET", "/api/v1/prices/{}/{}-{}_SE3.json".format(year, month, day), payload)
     res = conn.getresponse()
 
     data = json.loads(res.read().decode())
